0fe1555b2f784f23a530f0d6b488d28ae3729fd5
joshcv2112 <joshcv2112@gmail.com>
2016-09-28 17:07:08 -0600

    Created Absolute Value program.

---
+++ Chapter4/AbsoluteValue.java
@@ -0,0 +1,28 @@
+//************************************************************
+// Author: Malik/Burton
+// This program determines the absolute value of an int value.
+//************************************************************
+
+import javax.swing.JOptionPane;
+
+public class AbsoluteValue
+{
+    public static void main(String[] args)
+    {
+        int number;
+        int temp;
+
+        String numString;
+
+        numString = JOptionPane.showInputDialog("Enter an integer: ");
+
+        number = Integer.parseInt(numString);
+        temp = number;
+
+        if (number < 0)
+            number = -number;
+        
+        JOptionPane.showMessageDialog(null, "The absolute value of " + temp + " is " + number, "Absolute Value", JOptionPane.INFORMATION_MESSAGE);
+        System.exit(0);
+    }
+}
