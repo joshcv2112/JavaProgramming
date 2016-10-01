3ee95780575d59f2915ef55026a19c2b629931da
Joshua Vaughan <joshcv2112@gmail.com>
2016-10-01 14:20:43 -0600

    Added buttons to CalculatorGUI program

--- CalculatorGUI/Calculator.java
+++ CalculatorGUI/Calculator.java
@@ -11,88 +11,102 @@ import java.awt.event.*;
 
 public class Calculator extends JFrame
 {
+    private JPanel panel1, panel2, panel3, panel4, panel5, panel6;
     private JTextField textField;
     private JButton addB, subB, multB, divB, equalsB, dotB;
-    private JButton oneB, twoB, threeB, fourB, fiveB,
-                    sixB, sevenB, eightB, nineB, zeroB;
+    private JButton[] numberButtons = new JButton[10];
     private JButton clearB, exitB;
+    private ButtonHandler buttonHandler;
 
     private Container pane;
     private static final int WIDTH = 300;
     private static final int HEIGHT = 500;
 
+    private void setButtons()
+    {
+        for (int i = 0; i < 10; i++)
+            numberButtons[i] = new JButton(Integer.toString(i));
+        
+        addB = new JButton("+");
+        subB = new JButton("-");
+        multB = new JButton("*");
+        divB = new JButton("/");
+        equalsB = new JButton("=");
+        dotB = new JButton(".");
+        clearB = new JButton("C");
+        exitB = new JButton("Exit");
+    }
+
+    private void setButtonsHandler()
+    {
+        for (int i = 0; i < 10; i++)
+            numberButtons[i].addActionListener(buttonHandler);
+        
+        addB.addActionListener(buttonHandler);
+        subB.addActionListener(buttonHandler);
+        multB.addActionListener(buttonHandler);
+        divB.addActionListener(buttonHandler);
+        dotB.addActionListener(buttonHandler);
+        equalsB.addActionListener(buttonHandler);
+        clearB.addActionListener(buttonHandler);
+        exitB.addActionListener(buttonHandler);
+    }
+
     public Calculator()
     {
+        buttonHandler = new ButtonHandler();
+        // Add buttons
+        setButtons();
+        setButtonsHandler();
+
+        
         // START panel1 setup
         textField = new JTextField("", 60);
         textField.setHorizontalAlignment(JTextField.CENTER);
 
-        JPanel panel1 = new JPanel();
+        panel1 = new JPanel();
         panel1.setLayout(new GridLayout(1, 1));
         panel1.add(textField);
         // END Panel1 setup.
 
         // START Panel 2 setup.
-        oneB = new JButton("1");
-        twoB = new JButton("2");
-        threeB = new JButton("3");
-        addB = new JButton("+");
-
-        JPanel panel2 = new JPanel();
+        panel2 = new JPanel();
         panel2.setLayout(new GridLayout(1, 4));
-        panel2.add(oneB);
-        panel2.add(twoB);
-        panel2.add(threeB);
+        panel2.add(numberButtons[1]);
+        panel2.add(numberButtons[2]);
+        panel2.add(numberButtons[3]);
         panel2.add(addB);
         // END Panel 2 setup.
 
         // START Panel 3 setup.
-        fourB = new JButton("4");
-        fiveB = new JButton("5");
-        sixB = new JButton("6");
-        subB = new JButton("-");
-
-        JPanel panel3 = new JPanel();
+        panel3 = new JPanel();
         panel3.setLayout(new GridLayout(1, 4));
-        panel3.add(fourB);
-        panel3.add(fiveB);
-        panel3.add(sixB);
+        panel3.add(numberButtons[4]);
+        panel3.add(numberButtons[5]);
+        panel3.add(numberButtons[6]);
         panel3.add(subB);
         // END Panel 3 setup.
 
         // START Panel 4 setup.
-        sevenB = new JButton("7");
-        eightB = new JButton("8");
-        nineB = new JButton("9");
-        multB = new JButton("*");
-
-        JPanel panel4 = new JPanel();
+        panel4 = new JPanel();
         panel4.setLayout(new GridLayout(1, 4));
-        panel4.add(sevenB);
-        panel4.add(eightB);
-        panel4.add(nineB);
+        panel4.add(numberButtons[7]);
+        panel4.add(numberButtons[8]);
+        panel4.add(numberButtons[9]);
         panel4.add(multB);
         // END Panel 4 setup.
 
         // START Panel 5 setup.
-        zeroB = new JButton("0");
-        dotB = new JButton(".");
-        equalsB = new JButton("=");
-        divB = new JButton("/");
-
-        JPanel panel5 = new JPanel();
+        panel5 = new JPanel();
         panel5.setLayout(new GridLayout(1, 4));
-        panel5.add(zeroB);
+        panel5.add(numberButtons[0]);
         panel5.add(dotB);
         panel5.add(equalsB);
         panel5.add(divB);
         // END Panel 5 setup.
 
         // START Panel 6 setup.
-        clearB = new JButton("C");
-        exitB = new JButton("Exit");
-
-        JPanel panel6 = new JPanel();
+        panel6 = new JPanel();
         panel6.setLayout(new GridLayout(1, 2));
         panel6.add(clearB);
         panel6.add(exitB);
@@ -117,7 +131,10 @@ public class Calculator extends JFrame
     {
         public void actionPerformed(ActionEvent e)
         {
+            String str = e.getActionCommand();
 
+            if (str.equals("Exit"))
+                System.exit(0);
         }
     }
 }
